8b9ec10c3a1046aa89d5db65eff5f51bf6386b23
Thomas Torsney-Weir <torsneyt@gmail.com>
2021-02-03 12:54:54 +0000

    clean out last year's coursework template

--- md_build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-pandoc -t latex -o renamewithstudentnumber.pdf -s submission.md \
-  part1/design1.md part1/design2.md part1/design3.md part1/design4.md part1/design5.md \
-  part2/treemap1.md part2/treemap2.md part3/p3.md
